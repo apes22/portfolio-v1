2bc868fea8f4fce474e2644c4cb1093e51f118d2
Maribel Duran <maribelduranucm@gmail.com>
2017-05-16 11:29:07 -0700

    Cleaned up file to have all event listeners be in the view.setUpEventListeners method.

--- js/index.js
+++ js/index.js
@@ -1,54 +1,26 @@
-$(document).ready(function(){   
-    $(".arrow").addClass("animated bounce"); 
-});
-
-$(document).ready(function(){   
-    $("h1").addClass("animated bounceInDown"); 
-});
-
-//create a jquery function that will change scroll down arrow to 'SCROLL DOWN' 
-//text when the mouse isover the arrow.
-$(document).ready(function(){
-	$('#arrow').on('hover',function (e) {
-	    //e.preventDefault();
-
-	    var target = this.hash;
-	    var $target = $(target);	    
-	});
-});
-
-/*Used to close responsive navbar automatically. When this jquery event method is not applied, you have to manually close it after opening. Does not work for submenus.*/
-$('.navbar-collapse a').click(function(){
-    $(".navbar-collapse").collapse('hide');
-});
-
 var view = {
 	setUpEventListeners: function(){
-		window.addEventListener("scroll", function(){
+		
+		$(window).scroll(function(){
 			if (document.body.scrollTop > 75){
-
-				$('.navbar').css("background-color", "white");
-				$('.navbar').css("border-bottom", "2px solid #cccccc");
-				
-				$('.navbar-nav>li>a').css("color", "#337ab7");
-				$('.navbar-brand').css("color", "#337ab7");
-				$('.icon-bar').css("background-color", "#337ab7");
+				$('.navbar').addClass('white-background navbar-border');
+				$('.navbar-nav>li>a, .navbar-brand').addClass('bootstrap-blue');
+				$('.icon-bar').addClass('bootstrap-background-color');
 			}
 			else{
-				$('.navbar').css("background-color", "transparent");
-				$('.navbar').css("border-bottom", "none");
-
-				$('.navbar-nav>li>a').css("color", "white");
-				$('.navbar-brand').css("color", "white");
-				$('.icon-bar').css("background-color", "white");
+				$('.navbar').removeClass('white-background navbar-border');
+				$('.navbar-nav>li>a, .navbar-brand').removeClass('bootstrap-blue');
+				$('.icon-bar').removeClass('bootstrap-background-color');
 			}
-    	});
-}
-};
+		});
+		/*Used to close responsive navbar automatically. When this jquery event 
+		method is not applied, you have to manually close it after opening. Does not work for submenus.*/
+		$('.navbar-collapse a').click(function(){
+    	$(".navbar-collapse").collapse('hide');
+		});
 
-//Performs a smooth page scroll to an anchor on the same page.
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
+		//Performs a smooth page scroll to an anchor on the same page.
+		$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
 
 	    var target = this.hash;
@@ -59,16 +31,8 @@ $(document).ready(function(){
 	    }, 1000, 'swing', function () {
 	       window.location.hash = target;
 	    });
-	});
-});
-
-function updateNavColor(){
-	if (document.body.scrollTop > 75){
-		$('.navbar').css("background-color", "white");
+		});
 	}
-	else{
-		$('.navbar').css("background-color", "transparent");
-	}
-}
+};
 
 view.setUpEventListeners();
